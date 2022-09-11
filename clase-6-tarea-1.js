5e1b5fbe5f8ed19d771ca61bc70009710dd4f6ff
fktz94 <106633973+fktz94@users.noreply.github.com>
2022-09-11 13:01:15 -0300

    Creacion formulario
    Primer paso de la consigna. Según el número que ingresa el usuario se crean la cantidad de inputs, con sus respectivos ID y clases, a través de JS (DOM). También se creo archivo .css y se dieron los primeros estilos.

--- clase-6-tarea-1.js
+++ clase-6-tarea-1.js
@@ -6,3 +6,39 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+function calcularEdades() {
+    const miembrosDelGrupoFamiliar = Number(prompt('¿cuantas personas hay en su grupo familiar?'));
+    const $divInputs = document.querySelector('#inputs');
+
+    for (let i = 1; i <= miembrosDelGrupoFamiliar; i++) {
+
+        const labelNombre = document.createElement('label');
+        labelNombre.textContent = [i] + ') Nombre del familiar:';
+        labelNombre.className = 'label-nombre';
+        $divInputs.appendChild(labelNombre);
+        
+        const inputNombre = document.createElement('input');
+        inputNombre.type = 'text';
+        inputNombre.className = 'input-nombre';
+        $divInputs.appendChild(inputNombre);
+        
+        const labelEdad = document.createElement('label');
+        labelEdad.textContent = 'Edad: '
+        labelEdad.className = 'label-edad';
+        $divInputs.appendChild(labelEdad);
+        
+        const inputEdad = document.createElement('input');
+        inputEdad.type = 'number';
+        inputEdad.className = 'input-edad';
+        inputEdad.id = `input-edad-${i}`
+        $divInputs.appendChild(inputEdad);
+        
+        const br = document.createElement('br');
+        $divInputs.appendChild(br);
+    }
+
+
+}
+calcularEdades();
+
+
