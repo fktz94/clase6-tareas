1e7e92a4beaa5d82c5fd61818e02c75e5c14d2bd
fktz94 <106633973+fktz94@users.noreply.github.com>
2022-09-12 12:01:14 -0300

    Primera aproximación a botones
    Se cambiaron los nombres de los archivos y se agregó la tarea dos. Primer aproximación a intentar indexar el input de salario

--- clase-6-tarea.js
+++ clase-6-tarea.js
@@ -6,7 +6,20 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
-function calcularEdades() {
+
+
+/*
+TAREA:
+Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
+Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+
+Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+*/
+
+
+
+
+function clase6() {
     const miembrosDelGrupoFamiliar = Number(prompt('¿cuantas personas hay en su grupo familiar?'));
     const $divInputs = document.querySelector('#inputs');
 
@@ -37,11 +50,9 @@ function calcularEdades() {
         $divInputs.appendChild(br);
     }
 
-    const $form = document.getElementById('form');
-    
     const $botonCalcular = document.querySelector('#boton-calcular');
-
     $botonCalcular.onclick = function () {
+
         let arrayEdades = [];
         let contadorEdades = 0;
 
@@ -49,22 +60,53 @@ function calcularEdades() {
             let $inputEdad = Number(document.querySelector(`#input-edad-${i}`).value);
             arrayEdades.push($inputEdad);
 
-            contadorEdades += Number(document.querySelector(`#input-edad-${i}`).value); 
-        }   
+            contadorEdades += Number(document.querySelector(`#input-edad-${i}`).value);
+        }
         const mayor = Math.max(...arrayEdades);
         const textMayor = document.createTextNode(`La edad mayor es ${mayor}`)
         document.querySelector('#edad-mayor').appendChild(textMayor);
-        
+
         const menor = Math.min(...arrayEdades);
         const textMenor = document.createTextNode(`La edad menor es ${menor}`)
         document.querySelector('#edad-menor').appendChild(textMenor);
-        
+
         let promedio = contadorEdades / miembrosDelGrupoFamiliar;
-        promedio = promedio.toFixed(2)
+        promedio = promedio.toFixed()
         const textPromedio = document.createTextNode(`La edad promedio del grupo familiar es ${promedio}`);
         document.querySelector('#promedio-edad').appendChild(textPromedio);
     }
-}
-calcularEdades();
 
+
+    function resetearTodo() {
+        document.getElementById('#inputs').textContent = null;
+    }
+    document.getElementById('form').addEventListener('reset', resetearTodo);
+
+
+
+    function crearBotones() {
+        const $divBotones = document.querySelector('.botones-salario');
+        const $botonAgregar = document.getElementById('boton-agregar');
+        i = 0;
+
+        $botonAgregar.onclick = function () {
+            i++;
+            const labelSalario = document.createElement('label');
+            labelSalario.textContent = 'Salario anual';
+            const inputSalario = document.createElement('input');
+            inputSalario.type = 'number';
+            inputSalario.id = `input-salario-${i}`;
+            $divInputs.appendChild(labelSalario);
+            $divInputs.appendChild(inputSalario);
+            console.log(inputSalario)
+            
+        }
+
+
+    }
+    crearBotones()
+
+
+}
+clase6();
 
