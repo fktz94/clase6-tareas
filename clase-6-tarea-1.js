4c80e97b27419ca97bc82c97e1ae0ae3d043de75
fktz94 <106633973+fktz94@users.noreply.github.com>
2022-09-11 23:50:54 -0300

    Resolución tarea
    Se crean la función para crear el array y las tareas planteadas. Falta resolver el botón de reset.

--- clase-6-tarea-1.js
+++ clase-6-tarea-1.js
@@ -16,28 +16,54 @@ function calcularEdades() {
         labelNombre.textContent = [i] + ') Nombre del familiar:';
         labelNombre.className = 'label-nombre';
         $divInputs.appendChild(labelNombre);
-        
+
         const inputNombre = document.createElement('input');
         inputNombre.type = 'text';
         inputNombre.className = 'input-nombre';
         $divInputs.appendChild(inputNombre);
-        
+
         const labelEdad = document.createElement('label');
         labelEdad.textContent = 'Edad: '
         labelEdad.className = 'label-edad';
         $divInputs.appendChild(labelEdad);
-        
+
         const inputEdad = document.createElement('input');
         inputEdad.type = 'number';
         inputEdad.className = 'input-edad';
         inputEdad.id = `input-edad-${i}`
         $divInputs.appendChild(inputEdad);
-        
+
         const br = document.createElement('br');
         $divInputs.appendChild(br);
     }
 
+    const $form = document.getElementById('form');
+    
+    const $botonCalcular = document.querySelector('#boton-calcular');
 
+    $botonCalcular.onclick = function () {
+        let arrayEdades = [];
+        let contadorEdades = 0;
+
+        for (let i = 1; i <= miembrosDelGrupoFamiliar; i++) {
+            let $inputEdad = Number(document.querySelector(`#input-edad-${i}`).value);
+            arrayEdades.push($inputEdad);
+
+            contadorEdades += Number(document.querySelector(`#input-edad-${i}`).value); 
+        }   
+        const mayor = Math.max(...arrayEdades);
+        const textMayor = document.createTextNode(`La edad mayor es ${mayor}`)
+        document.querySelector('#edad-mayor').appendChild(textMayor);
+        
+        const menor = Math.min(...arrayEdades);
+        const textMenor = document.createTextNode(`La edad menor es ${menor}`)
+        document.querySelector('#edad-menor').appendChild(textMenor);
+        
+        let promedio = contadorEdades / miembrosDelGrupoFamiliar;
+        promedio = promedio.toFixed(2)
+        const textPromedio = document.createTextNode(`La edad promedio del grupo familiar es ${promedio}`);
+        document.querySelector('#promedio-edad').appendChild(textPromedio);
+    }
 }
 calcularEdades();
 
